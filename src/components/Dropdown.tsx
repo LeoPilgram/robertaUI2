45f85c72c9049eb6439aeaddd1c18ea58ebed389
LeoPilgram <leonhardpilgram@gmail.com>
2024-05-13 10:18:54 +0200

    change dropdown to select

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,32 +1,15 @@
-'use client';
-import React, { useState } from 'react';
-
 function DropdownMenu() {
-  const [selectedItem, setSelectedItem] = useState<string | null>(
-    null
-  );
-
-  const handleItemClick = (item: string) => {
-    setSelectedItem(item);
-  };
-
   return (
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn m-1">
-        {selectedItem ? selectedItem : 'Click'}
-      </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a onClick={() => handleItemClick('Item 1')}>Item 1</a>
-        </li>
-        <li>
-          <a onClick={() => handleItemClick('Item 2')}>Item 2</a>
-        </li>
-      </ul>
-    </div>
+    <select className="select w-full max-w-xs">
+      <option disabled selected>
+        Pick your favorite Simpson
+      </option>
+      <option>Homer</option>
+      <option>Marge</option>
+      <option>Bart</option>
+      <option>Lisa</option>
+      <option>Maggie</option>
+    </select>
   );
 }
 
