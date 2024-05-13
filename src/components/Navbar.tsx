6c1685df54f68a33cf4621642bb1c908d17b9b8e
izzlevershizzle <s_kaspar21@stud.hwr-berlin.de>
2024-05-13 13:58:36 +0200

    Ich habe die Navbar so verändert, dass sie jetzt eine Komponente mit Daisy UI ist.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,51 +1,59 @@
-'use client';
-
-import { NAV_LINKS } from '@/constants';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
-
-// Source: https://github.com/adrianhajdin/travel_ui_ux/blob/main/components/Navbar.tsx
 const Navbar = () => {
-  const pathname = usePathname();
   return (
-    <nav className="bg-rich-50 flex flex-row justify-between items-center">
-      <div className="p-3">
-        <Image
-          src="/robertaLogo.png"
-          width={50}
-          height={50}
-          alt="Picture of the author"
-        />
-      </div>
-      <div className="p-4 flex">
-        <div className=" w-3/4 space-x-20">
-          {NAV_LINKS.map((link) => (
-            <Link
-              key={link.key}
-              href={link.href}
-              className={clsx(
-                'rounded-md bg-rich-70 p-3 text-sm font-medium hover:bg-blue-100 hover:text-blue-600 md:flex-none',
-                {
-                  'text-blue-600': pathname === link.href,
-                }
-              )}
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {link.label}
-            </Link>
-          ))}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a href="/">Homepage</a>
+            </li>
+            <li>
+              <a href="/delivery">Delivery</a>
+            </li>
+            <li>
+              <a href="/entertainment">Entertainment</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className=" p-3">
-        <Image
-          src="/robertaLogo.png"
-          width={50}
-          height={50}
-          alt="Picture of the author"
-        />
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Roberta UI</a>
       </div>
-    </nav>
+      <div className="navbar-end">
+        <a href="/">
+          <button className="btn btn-ghost btn-circle">
+            <img
+              src="/robertaLogo.png"
+              alt="Roberta Logo"
+              className="h-6 w-6"
+            />
+          </button>
+        </a>
+      </div>
+    </div>
   );
 };
 
