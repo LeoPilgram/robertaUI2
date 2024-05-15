e1e18d143401c7560facb6ccd1dc3df91aa77162
LeoPilgram <leonhardpilgram@gmail.com>
2024-05-15 13:40:54 +0200

    add footerlinks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="footer flex justify-center items-center bg-base-300">
+    <footer className="footer flex justify-around items-center bg-base-300">
       <a
         target="_blank"
         href="https://github.com/LeoPilgram/robertaUI2"
@@ -15,7 +15,46 @@ const Footer = () => {
           src="/github-mark.svg"
           width={50}
           height={50}
-          alt="Picture of the author"
+          alt="Github Icon"
+        />
+      </a>
+      <a
+        target="_blank"
+        href="https://www.hwr-berlin.de/"
+        rel="noopener noreferrer"
+        className="p-3"
+      >
+        <Image
+          src="/HWR.png"
+          alt="HWR Logo"
+          width={200}
+          height={200}
+        />
+      </a>
+      <a
+        target="_blank"
+        href="http://wi-se.hwr-berlin.de/confluence/display/SES24/Software+Engineeering+-+Summer+2024+Home"
+        rel="noopener noreferrer"
+        className="p-3"
+      >
+        <Image
+          src="/confluence.png"
+          alt="Confluence Logo"
+          width={160}
+          height={160}
+        />
+      </a>
+      <a
+        target="_blank"
+        href="http://wi-se.hwr-berlin.de/jira/secure/RapidBoard.jspa?rapidView=27&projectKey=SES24"
+        rel="noopener noreferrer"
+        className="p-3"
+      >
+        <Image
+          src="/jira.png"
+          alt="Jira Logo"
+          width={70}
+          height={70}
         />
       </a>
     </footer>
