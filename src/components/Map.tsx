fc27d44c84c7b0a9aab7286219f733f8edd1372f
LeoPilgram <leonhardpilgram@gmail.com>
2024-06-08 17:49:52 +0200

    add drawer and map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,9 @@
+const Map = () => {
+  return (
+    <div className="bg-red-300 flex grow ml-5 mr-5 items-center justify-center">
+      Map - Placeholder
+    </div>
+  );
+};
+
+export default Map;
