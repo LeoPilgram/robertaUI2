cde9f42136e593a630708b2a40c555afda313eea
LeoPilgram <leonhardpilgram@gmail.com>
2024-06-22 19:36:00 +0200

    delete async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           </form> */}
           <button
             className="btn btn-primary"
-            onClick={async () => {
+            onClick={() => {
               runTest();
             }}
           >
