57fba23ffc34469c7186ad625362c2ae926228aa
LeoPilgram <leonhardpilgram@gmail.com>
2024-07-01 15:59:09 +0200

    Change script

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,15 +3,18 @@ import { exec } from 'child_process';
 
 export async function runTest(): Promise<void> {
   return new Promise((resolve, reject) => {
-    exec('src/app/scripts/test.sh', (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-        return;
+    exec(
+      'sshpass -p "turtlebot" ssh ubuntu@192.168.50.53 "bash -s" < src/app/scripts/test.sh',
+      (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+          return;
+        }
+        console.log(stdout);
+        resolve();
       }
-      console.log(stdout);
-      resolve();
-    });
+    );
   });
 }
 export async function dance3(): Promise<void> {
