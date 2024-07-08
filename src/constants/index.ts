f4e6cf9055fef909376c50e9cee0e8d4f63045d6
LeoPilgram <leonhardpilgram@gmail.com>
2024-07-08 10:28:31 +0200

    change names

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,20 +10,20 @@ export const NAV_LINKS = [
   { href: '/services', key: 'services', label: 'Services' },
 ];
 export const DANCES = [
-  { id: 0, name: 'Dance 1' },
-  { id: 1, name: 'Dance 2' },
-  { id: 2, name: 'Dance 3' },
+  { id: 0, name: 'Dance_1' },
+  { id: 1, name: 'Dance_2' },
+  { id: 2, name: 'Dance_3' },
 ];
 export const SONGS = [
-  { id: 0, name: 'Track 1' },
-  { id: 1, name: 'Track 2' },
-  { id: 2, name: 'Track 3' },
+  { id: 0, name: 'Track_1' },
+  { id: 1, name: 'Track_2' },
+  { id: 2, name: 'Track_3' },
 ];
 export const CHECKPOINTS = [
-  { id: 0, name: 'Checkpoint 1' },
-  { id: 1, name: 'Checkpoint 2' },
-  { id: 2, name: 'Checkpoint 3' },
-  { id: 0, name: 'Checkpoint 4' },
-  { id: 1, name: 'Checkpoint 5' },
-  { id: 2, name: 'Checkpoint 6' },
+  { id: 0, name: 'Checkpoint_1' },
+  { id: 1, name: 'Checkpoint_2' },
+  { id: 2, name: 'Checkpoint_3' },
+  { id: 0, name: 'Checkpoint_4' },
+  { id: 1, name: 'Checkpoint_5' },
+  { id: 2, name: 'Checkpoint_6' },
 ];
